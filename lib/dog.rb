96823da6c64ab0bd62d5544f8e62bcace1b14493
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-23 22:07:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,12 @@
 require_relative "person.rb"
 class Dog
+  
   attr_accessor :breed
   attr_reader = :name
+  
   def initialize(name, breed = 'Mutt')
      @name = name
      @breed = breed
-end 
+  end 
+  
 end
-fido = Person.new('Fido','')
